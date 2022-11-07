b8f8dafadb98a31067faef2edffc1524bb5bce89
frankleitelemoscosta <frankleite090@gmail.com>
2022-11-07 11:42:31 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//inicio das inclusões de biblioteca
+#include<iostream>
+using namespace std;
+//fim das inclusões de biblioteca
+
+int main()
+{
+    int op;
+
+    do{
+
+        cout<<"|---------------MENU-----------------|"<<endl;
+        cout<<"|1- Inserir um individuo na fila     |"<<endl;
+
+    }while(op!=0);
+
+
+    return 0;
+}
